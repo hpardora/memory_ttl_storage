40548c2f00f020c5218bca1f1c30c4ba1dd27fa1
Pardo <hpardo@unnax.com>
2021-09-21 18:35:01 +0200

    Initial code commit

---
+++ mtstorage.go
@@ -0,0 +1,110 @@
+package memory_ttl_storage
+
+import (
+	"fmt"
+	"log"
+	"time"
+)
+
+const (
+	defaultTickerTime = time.Second * 1
+	defaultTTL        = int64(10)
+)
+
+type Item struct {
+	Content         interface{}
+	ExpireTimestamp int64
+	TTL             int64
+}
+
+type MemoryTTLStorage struct {
+	showLogs   bool
+	ticker     time.Ticker
+	items      map[string]Item
+	defaultTTL int64
+}
+
+func New(tickerTime *time.Duration, ttlValue *int64, showLogs bool) *MemoryTTLStorage {
+	finalTickerTime := defaultTickerTime
+	if tickerTime != nil {
+		finalTickerTime = *tickerTime
+	}
+	var finalTTLValue = defaultTTL
+	if ttlValue != nil {
+		finalTTLValue = *ttlValue
+	}
+
+	rlc := MemoryTTLStorage{
+		showLogs:   showLogs,
+		defaultTTL: finalTTLValue,
+		items:      make(map[string]Item),
+	}
+
+	rlc.log(fmt.Sprintf("creating a MemoryTTLStorage with tickerTime %d/s and default TTL %d/s", finalTickerTime/time.Second, finalTTLValue))
+
+	t := time.NewTicker(finalTickerTime)
+	quit := make(chan struct{})
+	go func() {
+		for {
+			select {
+			case <-t.C:
+				rlc.clearOldEntries()
+			case <-quit:
+				t.Stop()
+				return
+			}
+		}
+	}()
+	rlc.ticker = *t
+	return &rlc
+}
+
+func (r *MemoryTTLStorage) Stop() {
+	r.ticker.Stop()
+}
+
+func (r *MemoryTTLStorage) clearOldEntries() {
+	for k, v := range r.items {
+		if v.ExpireTimestamp < time.Now().Unix() {
+			r.log("deleting outdated item", k)
+			delete(r.items, k)
+		}
+	}
+}
+
+func (r *MemoryTTLStorage) log(v ...interface{}) {
+	if r.showLogs {
+		data := v
+		log.Println(data)
+	}
+}
+
+func (r *MemoryTTLStorage) SetDefaultTTL(defaultTTL int64) {
+	r.defaultTTL = defaultTTL
+	r.log("defaultTTL updated", defaultTTL)
+}
+
+func (r *MemoryTTLStorage) Get(key string) (*Item, bool) {
+	val, ok := r.items[key]
+	return &val, ok
+}
+
+func (r *MemoryTTLStorage) Delete(key string) {
+	delete(r.items, key)
+	r.log("deleted element with key", key)
+}
+
+func (r *MemoryTTLStorage) Add(key string, content interface{}, ttl *int64) {
+	finalTTL := r.defaultTTL
+	if ttl != nil {
+		finalTTL = *ttl
+	}
+
+	expirationTS := time.Now().Unix() + finalTTL
+	i := Item{
+		Content:         content,
+		ExpireTimestamp: expirationTS,
+		TTL:             finalTTL,
+	}
+	r.items[key] = i
+}
---
+++ mtstorage_test.go
@@ -0,0 +1,67 @@
+package memory_ttl_storage
+
+import (
+	"testing"
+	"time"
+)
+
+type TestStructOne struct {
+	One int
+	Two string
+}
+
+type TestStructTwo struct {
+	Three int
+	Four string
+}
+
+func TestAddDefaultTTL(t *testing.T){
+	mts := New(nil, nil,false)
+	ts := &TestStructOne{}
+	keyTest := "test_key"
+	mts.Add(keyTest, ts, nil)
+	i, ok := mts.Get(keyTest)
+	if !ok {
+		t.Error("cannot retrieve from service")
+	}
+	if i.TTL != defaultTTL {
+		t.Error("stored TTL is not de defaultTTL")
+	}
+}
+
+func TestAddAndRetrieveAnObject(t *testing.T){
+	mts := New(nil, nil,false)
+
+	keyTestOne := "test_key_one"
+	tsOne := &TestStructOne{One: 1, Two: "two"}
+
+	keyTestTwo := "test_key_two"
+	tsTwo := &TestStructTwo{Three: 3, Four: "four"}
+
+	mts.Add(keyTestOne, tsOne, nil)
+	mts.Add(keyTestTwo, tsTwo, nil)
+
+	i, ok := mts.Get(keyTestOne)
+	if !ok {
+		t.Error("cannot retrieve from service")
+	}
+	returned := i.Content.(*TestStructOne)
+
+	if tsOne.One != returned.One {
+		t.Errorf("Unexpected respose! Expected %d, got %d", tsOne.One, returned.One)
+	}
+}
+
+func TestElementDontExistsAfterTTL(t *testing.T){
+	desairedTTL := int64(1)
+	mts := New(nil, &desairedTTL,false)
+	ts := &TestStructOne{}
+	keyTest := "test_key"
+	mts.Add(keyTest, ts, nil)
+	time.Sleep(time.Second * 3)
+	_, ok := mts.Get(keyTest)
+	if ok {
+		t.Error("you should not be able to see retrieve this element")
+	}
+
+}
